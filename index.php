a33cf00f48926c93cb2bb696f6ac226628a0d55f
Leondk256 <leondekraker@hotmail.com>
2019-09-02 08:54:50 +0200

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include('header.php');
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Portfolio Leon - Home
+    </title>
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ profile.php
@@ -0,0 +1,90 @@
+<?php
+include('header.php');
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Portfolio Leon - Profiel
+    </title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <img src="img/hz-logo.png" class="img-fluid" alt="HZ logo">
+            </div>
+            <div class="col-9 mt-4">
+                < bien d'r nu alweer klaer mee
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-9 mt-5">
+                <ul>
+                    <li>
+                        Persoonlijke kenmerken:
+                    </li>
+                    <ul>
+                        <li> Sociaal </li>
+                        <li> Behulpzaam </li>
+                        <li> Begripvol </li>
+                    </ul>
+                </ul>
+
+                <ul>
+                    <li>
+                        Interesses
+                    </li>
+                    <ul>
+                        <li> Gamen </li>
+                        <ul>
+                            <li>
+                                RPG's/action RPG's
+                            </li>
+                            <li>
+                                Hack and slash
+                            </li>
+                            <li>
+                                Shooters (tot op zekere hoogte)
+                            </li>
+                            <li>
+                                etc...
+                            </li>
+                        </ul>
+                        <li> Sporten </li>
+                        <ul>
+                            <li>
+                                Tennis
+                            </li>
+                            <li>
+                                Joggen
+                            </li>
+                        </ul>
+                        <li> Muziek luisteren </li>
+                        <ul>
+                            <li>
+                                Rock
+                            </li>
+                            <li>
+                                Metal
+                            </li>
+                            <li>
+                                Klassiek
+                            </li>
+                            <li>
+                                Pop
+                            </li>
+                            <li>
+                                Jazz
+                            </li>
+                        </ul>
+                    </ul>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
